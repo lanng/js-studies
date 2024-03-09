84f8f389f8815270b37172a3b15564a1d5e4673e
Victor Languer <victorlanguer@hotmail.com>
2024-03-09 19:49:06 -0300

    day-7

---
+++ day-7/index.js
@@ -0,0 +1,52 @@
+const people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 },
+]
+
+const comments = [
+{ text: 'Love this!', id: 523423 },
+{ text: 'Super good', id: 823423 },
+{ text: 'You are the best', id: 2039842 },
+{ text: 'Ramen in my fav food ever', id: 123523 },
+{ text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// const isAdult = people.some(function(person) {
+//     const currentYear = (new Date()).getFullYear();
+//     if (currentYear - person.year >= 19){
+//         return true;
+//     }
+// })
+//with arrow fn
+
+const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19)
+console.log(isAdult)
+
+//is everyone 19?
+const isAdult2 = people.every(person => ((new Date()).getFullYear()) - person.year >= 19)
+console.log(isAdult2)
+
+// Array. prototype. find()
+// Find is like filter, but instead returns just the one you
+// are looking for
+// find the comment with the ID 823423:
+
+const comment = comments.find(comment => comment.id === 823423);
+console.log(comment)
+
+
+//Array. prototype. findIndex()
+// Find the comment with this ID
+// delete the comment with the ID 0f 823423
+
+const commentIndex = comments.findIndex(comment => comment.id === 823423)
+
+//comments.slice(commentIndex, 1);
+const newComments = [
+    ...comments.slice(0, commentIndex),
+    ...comments.slice(commentIndex + 1)
+];
+
+console.log(newComments);
